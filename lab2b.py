6e2125a0ee88f7999273e1b614b9fbf4d95ec438
Prince Ranjo <pranjo@myseneca.ca>
2024-05-17 12:06:25 -0400

    Completed Inv1

---
+++ lab2b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Create variables for user to input value
+name = input('Name: ')
+age = input('Age: ')
+
+# Prints out text
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
---
+++ lab2c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Create Variables that store arguments
+# ***arguments are stored as string objects***
+name = sys.argv[1]
+age = sys.argv[2]
+
+# Prints out text
+print('Hi ' + name + ', you are ' + age + ' years old.')
